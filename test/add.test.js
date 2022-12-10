c991f8acfa40fe855eb548688d260fc0a87507d8
joe <joe.chang1014@gmail.com>
2022-12-10 13:02:05 +0800

    fix jest folder

--- test/add.test.js
+++ test/add.test.js
@@ -1,7 +1,8 @@
-import { add } from "../test";
+import { add } from "../src/test";
 
 describe('calculator', () => {
     test('add 1 + 2' , () => {
         expect(add(1, 2)).toBe(3);
     })
 });
+
