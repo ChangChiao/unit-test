2b9b7f432a2ac5038e16904517a33ffa38e7c24a
joe <joe.chang1014@gmail.com>
2022-12-13 17:06:24 +0800

    feat quiz

--- src/quiz.js
+++ src/quiz.js
@@ -1,3 +1,29 @@
 export const add = (a, b) => {
-    return a + b
-}
+  return a + b;
+};
+
+export const getStr = (n) => {
+//   let arr = [];
+//   for (let i = 1; i <= n; i++) {
+//     arr.push(i);
+//   }
+  const arr = Array.from({length: n}, (v, i) => {
+    let num = i + 1;
+    let str = '';
+    // if(num % 3 === 0 && num % 7 === 0  && num % 5 === 0) return "fizzbuzzdizz";
+    // if(num % 3 === 0 && num % 7 === 0) return "fizzdizz";
+    // if(num % 5 === 0 && num % 7 === 0) return "buzzdizz";
+    // if(num % 3 === 0 && num % 5 === 0) return "fizzbuzz";
+    if(num % 3 === 0) {
+        str+="fizz";
+    }
+    if(num % 5 === 0){
+        str+="buzz";
+    }
+    if(num % 7 === 0){
+        str+="dizz";
+    }
+    return str ? str : num
+  })
+  return arr.join(" ");
+};
--- test/quiz.test.js
+++ test/quiz.test.js
@@ -1,5 +1,21 @@
-import { add } from "../src/quiz";
+import { add, getStr } from "../src/quiz";
 
-test("total", () => {
-    expect(add(2, 3)).toBe(5);
-});
+// test("total", () => {
+//     expect(add(2, 3)).toBe(5);
+// });
+
+// test("testGetStr", () => {
+//     expect(getStr(3)).toBe("1 2 3")
+// })
+
+// test("testGetStr", () => {
+//     expect(getStr(6)).toBe("1 2 fizz 4 5 fizz")
+// })
+
+// test("testGetStr", () => {
+//     expect(getStr(15)).toBe("1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz")
+// })
+
+test("testGetStr", () => {
+    expect(getStr(30)).toBe("1 2 fizz 4 buzz fizz dizz 8 fizz buzz 11 fizz 13 dizz fizzbuzz 16 17 fizz 19 buzz fizzdizz 22 23 fizz buzz 26 fizz dizz 29 fizzbuzz")
+})
